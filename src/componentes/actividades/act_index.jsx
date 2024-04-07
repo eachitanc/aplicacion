735b0731556c73f96237a47b8a6f2ac5e1231fa4
eachitanc <eachitanc@gmail.com>
2024-04-07 18:58:48 -0500

    server

--- src/componentes/actividades/act_index.jsx
+++ src/componentes/actividades/act_index.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Navbar from '../navbar';
 import Act_Table from './act_table';
 import Footer from '../footer';
 function Inicio() {
     return (
-        <div>
+        <Fragment>
             <Navbar />
             <Act_Table />
             <Footer />
-        </div>
+        </Fragment>
     );
 }
 export default Inicio;
